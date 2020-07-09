83749ac3412106a722df660e05f223b34674ea23
jiaxuanliang <evilg231321@qq.com>
2020-07-09 21:11:04 +0800

    dev

--- src/enums/user-manage.js
+++ src/enums/user-manage.js
@@ -3,17 +3,47 @@ export const subject = {
     company: '公司',
     agent: '代理人'
 }
+export const subjectMap = {
+    company: {
+        label: '公司',
+        value: 'company'
+    },
+    agent: {
+        label: '代理人',
+        value: 'agent'
+    }
+}
 
 // 销售渠道
 export const sale_channel = {
     online: '线上',
     offline: '线下'
 }
+export const saleChannelMap = {
+    online: {
+        label: '线上',
+        value: 'online'
+    },
+    offline: {
+        label: '线下',
+        value: 'offline'
+    }
+}
 
 // 服务费发票类型
 export const invoice_type = {
     vat_special_ticket: '增值税专票',
     non_vat_special_ticket: '非增值税专票'
+}
+export const invoiceTypeMap = {
+    vat_special_ticket: {
+        label: '增值税专票',
+        value: 'vat_special_ticket'
+    },
+    non_vat_special_ticket: {
+        label: '非增值税专票',
+        value: 'non_vat_special_ticket'
+    }
 }
 
 // 账户类型
@@ -23,3 +53,22 @@ export const account_type = {
     wechat_pay: '微信',
     other: '其它'
 }
+export const accountTypeMap = {
+    bank: {
+        label: '银行',
+        value: 'bank'
+    },
+    alipay: {
+        label: '支付宝',
+        value: 'alipay'
+    },
+    wechat_pay: {
+        label: '微信',
+        value: 'wechat_pay'
+    },
+    other: {
+        label: '其它',
+        value: 'other'
+    }
+}
+
