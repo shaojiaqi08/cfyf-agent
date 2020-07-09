4ff66f2019a2709040bd4574c43f11e18bd4cb93
jiaxuanliang <evilg231321@qq.com>
2020-07-09 20:51:10 +0800

    dev

--- src/apis/modules/index.js
+++ src/apis/modules/index.js
@@ -1,10 +1,16 @@
-import { get } from '../helper'
+import { get, post } from '../helper'
 
 export const getList = params => get('/announcement/get-sales-unread-announcement-list', params)
 
 export const getRoleDetail = params => get('/adm_roles/detail', params)
 
 export const getRolePermission = params => get('/adm_roles/permission-id-list', params)
+
+// 角色列表
+export const getRoleList = params => get('/management/sys_permission/sys_role/get_page_list', params)
+
+// 添加角色
+export const createRole = params => post('/management/sys_permission/sys_role/store', params)
 
 // 地区数据
 export const getRegion = params => get('/common/get_region_List', params)
