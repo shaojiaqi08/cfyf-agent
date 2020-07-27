6cf5d850a21879f23e98250a813e78d25b99b18c
jiaxuanliang <evilg231321@qq.com>
2020-07-27 11:42:28 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,7 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       '/api': {
-        // target: 'http://proxy.tc.2239.com/web/v1',   //代理接口
-        target: process.env.VUE_APP_BASE_URL,//代理接口
+        target: 'http://proxy.tc.2239.com/web/v1',   //代理接口
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''    //代理的路径
