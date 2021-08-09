75f77f52263366bd6799e82fa6e069a74ada258c
raykit <1280268009@qq.com>
2021-08-09 11:39:42 +0800

    Fixed 同步内部系统改动

--- www/admin.yun.com/src/mixins/order-list-mixin.js
+++ www/admin.yun.com/src/mixins/order-list-mixin.js
@@ -4,16 +4,36 @@ import { debounce } from '@/utils'
 export default {
   methods: {
     rowStyleFormat(value) {
-      switch (value.row.policy_status) {
+      if (value.row.policy_manpower_underwriting && value.row.policy_manpower_underwriting.status == 0) {
+        return { color: `#09ba08` }
+      } else {
+        switch (value.row.policy_status) {
+          case policyStatus.UNPAID:
+            return { color: `#4497eb` }
+          case policyStatus.INVALID:
+          case policyStatus.CANCELED:
+            return { color: `#999999` }
+          case policyStatus.SURRENDERED:
+            return { color: `#FF4C4C` }
+          default:
+            return { color: `#000` }
+        }
+      }
+    },
+    getBgColor(value) {
+      console.log('---------', value)
+      switch (value) {
+        case 0:
+          return { background: `#09ba08` }
         case policyStatus.UNPAID:
-          return { color: `#4497eb` }
+          return { background: `#4497eb` }
         case policyStatus.INVALID:
         case policyStatus.CANCELED:
-          return { color: `#999999` }
+          return { background: `#999999` }
         case policyStatus.SURRENDERED:
-          return { color: `#FF4C4C` }
+          return { background: `#FF4C4C` }
         default:
-          return { color: `#000` }
+          return { background: `#000` }
       }
     },
     calcTableHeight: debounce(function() {
