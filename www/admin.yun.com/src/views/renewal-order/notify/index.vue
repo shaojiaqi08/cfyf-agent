24b34d596917562de5dfa925a833f40c124b8cf2
jiaxuanliang <evilg231321@qq.com>
2021-08-19 13:52:42 +0800

    renewal dev

--- www/admin.yun.com/src/views/renewal-order/notify/index.vue
+++ www/admin.yun.com/src/views/renewal-order/notify/index.vue
@@ -5,47 +5,68 @@
     </div>
     <div class="right-content">
       <div class="follow-head">
-        <span class="order-name">复星联合康乐一生重大疾病保超长…</span>
+        <el-tooltip content="复星联合康乐一生重大疾病保超长…" placement="top">
+          <span class="order-name">复星联合康乐一生重大疾病保超长…</span>
+        </el-tooltip>
         <el-dropdown>
           <span class="el-dropdown-link">
             本次续保<i class="el-icon-arrow-down el-icon--right"></i>
           </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>黄金糕</el-dropdown-item>
-            <el-dropdown-item>狮子头</el-dropdown-item>
+            <el-dropdown-item>a</el-dropdown-item>
+            <el-dropdown-item>b</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </div>
       <div class="step-wrap">
-        <div class="step-item">
-          <i></i>
-          <span>未跟踪</span>
-        </div>
-        <div class="step-item">
-          <i></i>
-          <span>跟踪中</span>
-        </div>
-        <div class="step-item">
-          <i></i>
-          <span>未联系上</span>
-        </div>
-        <div class="step-item">
-          <i></i>
-          <span>放弃续保</span>
-        </div>
-        <div class="step-item">
-          <i></i>
-          <span>其他</span>
+        <div
+          v-for="(item, index) in stepData"
+          :key="index"
+          class="step-item"
+          :class="{ active: index <= stepIndex }">
+          <i class="iconfont" :class="index <= stepIndex ? 'icona-zhong20_duigou_xuanzhong' : 'icona-zhong20_duigou_weixuanzhong'"></i>
+          <span>{{item.label}}</span>
         </div>
       </div>
-      <el-scrollbar></el-scrollbar>
+      <div class="year-wrap">2021</div>
+      <el-scrollbar class="scrollbar">
+        <div class="list-item">
+          <div class="date-wrap">
+            <span>08-08</span><br><span>09:33</span>
+          </div>
+          <div class="content-wrap">
+            <span class="title-wrap">
+              <i class="iconfont"></i>
+              <el-avatar v-if="false"></el-avatar>
+              系统触发短信
+            </span>
+            <div class="msg-wrap">
+              <p></p>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </el-scrollbar>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "renewal-notify"
+  name: 'renewal-notify',
+  data() {
+    return {
+      stepIndex: 2,
+      stepData: Object.freeze([
+        { label: '未跟踪', value: 'no_follow'},
+        { label: '跟踪中', value: 'following'},
+        { label: '未联系上', value: 'no_link'},
+        { label: '放弃续保', value: 'give_up_renewal'},
+        { label: '其他', value: 'other'}
+      ]),
+      list: [{}]
+    }
+  }
 }
 </script>
 
@@ -106,13 +127,90 @@ export default {
         flex-direction: column;
         flex: 1;
         overflow: hidden;
-        i {
-
+        align-items: center;
+        color: #ccc;
+        font-size: 12px;
+        .iconfont {
+          color: #CCCCCC;
+          font-size: 20px;
+          margin-bottom: 4px;
         }
         span {
           line-height: 17px;
         }
+        &.active {
+          color: #1F78FF;
+          .iconfont {
+            color: #1F78FF;
+          }
+        }
       }
+    }
+    .year-wrap {
+      height: 35px;
+      line-height: 35px;
+      color: #333;
+      font-size: 16px;
+      text-align: center;
+      border-top: 1px solid #E6E6E6;
+      border-bottom: 1px solid #E6E6E6;
+      font-weight: bold;
+    }
+    .el-scrollbar {
+      flex: 1;
+      overflow: hidden;
+      .list-item {
+        display: flex;
+        padding: 18px 0;
+        .date-wrap {
+          width: 72px;
+          height: 34px;
+          box-sizing: border-box;
+          border-right: 1px solid #d8d8d8;
+          color: #333;
+          text-align: center;
+          position: relative;
+          font-size: 12px;
+          span {
+            line-height: 17px;
+          }
+          &.active {
+            color: #1F78FF;
+          }
+        }
+        .content-wrap {
+          flex: 1;
+          font-size: 12px;
+          padding: 0 12px;
+          .title-wrap {
+            display: flex;
+            align-items: center;
+            color: #333;
+            transform: translateX(-20px);
+            height: 34px;
+            .iconfont {
+              color: #D8D8D8;
+              box-sizing: border-box;
+              border: 4px solid #fff;
+              margin-right: 2px;
+              width: 16px;
+              height: 16px;
+              border-radius: 50%;
+              background-color: #D8D8D8;
+            }
+          }
+          .msg-wrap {
+
+          }
+        }
+        &.active {
+
+        }
+      }
+    }
+    .follow-footer {
+      height: 84px;
+      overflow: hidden;
     }
   }
 }
