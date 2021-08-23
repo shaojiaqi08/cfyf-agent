8b5a507e4ecfe13ab70fa90d3d5053dabebc8500
jiaxuanliang <evilg231321@qq.com>
2021-08-23 16:24:05 +0800

    renewal dev

---
+++ www/admin.yun.com/src/views/renewal-order/renewal/dialog/common-dialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-dialog
+      :visible="visible"
+      title=""
+      @close="handleClose">
+    <div #footer>
+      <el-button @click="handleClose">取消</el-button>
+      <el-button @click="handleEnter">确定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'qrcode-dialog'
+  props: {
+    visible: Boolean
+  },
+  methods: {
+    handleEnter() {
+      this.$emit('handleSuccess')
+    },
+    handleClose() {
+      this.$emit('update:visible', false)
+    }
+  }
+}
+</script>
