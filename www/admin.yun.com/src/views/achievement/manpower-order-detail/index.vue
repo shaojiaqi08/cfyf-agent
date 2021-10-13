7391c94772151f53f7e7677f1bf8fad01ffcac86
秋了 <lzc.a@qq.com>
2021-10-13 11:46:09 +0800

    Fixed 修改

--- www/admin.yun.com/src/views/achievement/manpower-order-detail/index.vue
+++ www/admin.yun.com/src/views/achievement/manpower-order-detail/index.vue
@@ -82,6 +82,10 @@
                   class="content"
               >{{(baseInfo.policy_plan && baseInfo.policy_plan.guarantee_start_at) ? formatDate(baseInfo.policy_plan.guarantee_start_at * 1000, 'yyyy-MM-dd') : '-'}}</div>
             </div>
+            <div class="item">
+              <div class="label">承保失败原因：</div>
+              <div class="content">{{baseInfo.proposal_err_reason||'-'}}</div>
+            </div>
             <el-divider></el-divider>
           </div>
         </template>
