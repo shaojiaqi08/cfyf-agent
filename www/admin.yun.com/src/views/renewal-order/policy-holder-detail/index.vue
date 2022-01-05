f9f020d8f353c3a2eb2f84aaf00b277040cdbe85
junhao <1534682163@qq.com>
2022-01-05 19:41:00 +0800

    -

--- www/admin.yun.com/src/views/renewal-order/policy-holder-detail/index.vue
+++ www/admin.yun.com/src/views/renewal-order/policy-holder-detail/index.vue
@@ -57,7 +57,7 @@
             </el-table-column>
             <el-table-column label="预计保费" prop="actually_premium" align="center" width="100px"></el-table-column>
             <el-table-column label="应续日期" prop="current_renewal_stage.renewal_date_format" align="center" width="130px" ></el-table-column>
-            <el-table-column label="宽限日期" prop="grace_days_str" width="170px" align="center">
+            <el-table-column label="宽限日期" prop="current_renewal_stage.grace_days_str" width="170px" align="center">
               <!-- <template v-slot="{ row }">
                 <div v-if="row.current_renewal_stage.grace_start_at && row.current_renewal_stage.grace_end_at">
                   {{ formatDate(row.current_renewal_stage.grace_start_at * 1000, 'yyyyMMdd') }}
@@ -115,14 +115,14 @@
             </el-table-column>
             <el-table-column label="预计保费" prop="actually_premium" align="center" width="100px"></el-table-column>
             <el-table-column label="应续日期" prop="current_renewal_stage.renewal_date_format" align="center" width="130px" ></el-table-column>
-            <el-table-column label="宽限日期" width="170px" align="center">
-              <template v-slot="{ row }">
+            <el-table-column label="宽限日期" prop="current_renewal_stage.grace_days_str" width="170px" align="center">
+              <!-- <template v-slot="{ row }">
                 <div v-if="row.current_renewal_stage.grace_start_at && row.current_renewal_stage.grace_end_at">
                   {{ formatDate(row.current_renewal_stage.grace_start_at * 1000, 'yyyyMMdd') }}
                   -
                   {{ formatDate(row.current_renewal_stage.grace_end_at * 1000, 'yyyyMMdd') }}
                 </div>
-              </template>
+              </template> -->
           </el-table-column>
             <el-table-column label="操作" align="center" width="150px" fixed="right">
               <template slot-scope="{row}">
